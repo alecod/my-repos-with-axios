bfa3581a804b82812855e55d3dc861bd6d13ae4a
Ale Junqueira <alecodx@gmail.com>
2022-04-18 10:40:28 -0300

    update

--- src/pages/Repository/index.jsx
+++ src/pages/Repository/index.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
-import {Container} from './style'
+import {Container, Owner, Loading, BackButton, Issueslist, PageActions} from './style'
+import {FaArrowLeft} from 'react-icons/fa'
 import API from '../../services/api'
 
 
@@ -8,6 +9,7 @@ function Repository({match}) {
   const [repository, setRepository] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
 
   useEffect(() => {
@@ -38,12 +40,87 @@ function Repository({match}) {
 
     loadRepository()
 
-  }, [])
+  }, [match.params.repository])
+
+
+  useEffect(() => {
+
+
+    async function loadIssues() {
+      const nomeRepo = decodeURIComponent(match.params.repository)
+
+      const response = await API.get (`/repos/${nomeRepo})/issues`, {
+        params: {
+          state: 'open',
+          page: page,
+          per_page: 5,
+        }
+      })
+
+      setIssues(response.data);
+
+    }
+
+    loadIssues()
+
+  }, [match.params.repository, page])
+
+
+  function handlePage(action) {
+    setPage(action === 'back' ? page - 1 : page + 1);
+  }
+
+
+  if(loading) {
+    return (
+      <Loading>
+        <h1>Carregando....</h1>
+      </Loading>
+    )
+  }
 
   return (
     <Container>
-   
-      
+      <BackButton to="/">
+        <FaArrowLeft size={30} color="#222" />
+      </BackButton>
+
+      <Owner>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </Owner>
+
+      <Issueslist>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login}/>
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+          </li>
+
+        ))}
+
+      </Issueslist>
+
+      <PageActions>
+        <button type="button" onClick={() => handlePage('back')}>Voltar</button>
+        <button type="button" onClick={() => handlePage('next')}>Avançar</button>
+      </PageActions>
+
+
     </Container>
   )
 }
